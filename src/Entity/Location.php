0440abd85c63695a48345c034309f63ca028e09d
Schulc Daniel <sd46635>
2022-10-15 17:39:25 +0200

    Create entities

---
+++ src/Entity/Location.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LocationRepository::class)]
+class Location
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 510)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 8)]
+    private ?string $longitude = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 8)]
+    private ?string $latitude = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?string
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(string $longitude): self
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?string
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(string $latitude): self
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Measurement.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MeasurementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MeasurementRepository::class)]
+class Measurement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    private ?string $temperature = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    private ?string $wind_speed = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 9)]
+    private ?string $humidity = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getTemperature(): ?string
+    {
+        return $this->temperature;
+    }
+
+    public function setTemperature(string $temperature): self
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    public function getWindSpeed(): ?string
+    {
+        return $this->wind_speed;
+    }
+
+    public function setWindSpeed(string $wind_speed): self
+    {
+        $this->wind_speed = $wind_speed;
+
+        return $this;
+    }
+
+    public function getHumidity(): ?string
+    {
+        return $this->humidity;
+    }
+
+    public function setHumidity(string $humidity): self
+    {
+        $this->humidity = $humidity;
+
+        return $this;
+    }
+}
